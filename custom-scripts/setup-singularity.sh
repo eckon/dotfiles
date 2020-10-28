b3b790921e46ab9fee46cfafcb71d79f49eb428e
eckon <niklas.meyer95@hotmail.de>
2020-10-28 19:09:14 +0100

    Add comments

--- custom-scripts/setup-singularity.sh
+++ custom-scripts/setup-singularity.sh
@@ -13,6 +13,7 @@ if ! (tmux has-session -t $session 2>/dev/null); then
   tmux split-window -t $session:1 -p 20 -v -c $pathApi
   tmux send-key -t $session:1 'npm run start:dev' C-m
   tmux split-window -t $session:1 -h -c $pathApi
+  # set focus of this window (1) to the editor editor
   tmux select-pane -t $session:1 -U
 
   # handle frontend project view
@@ -21,12 +22,14 @@ if ! (tmux has-session -t $session 2>/dev/null); then
   tmux split-window -t $session:2 -p 20 -v -c $pathFrontend
   tmux send-key -t $session:2 'npm start' C-m
   tmux split-window -t $session:2 -h -c $pathFrontend
+  # set focus of this window (2) to the editor pane
   tmux select-pane -t $session:2 -U
 
   # handle backend structure view
   tmux new-window -t $session:3 -n 'Api-Docker' -c $pathApi
   tmux send-key -t $session:3 'docker-compose up' C-m
 
+  # set focus of the session to the first window
   tmux select-window -t $session:1
 fi
 
