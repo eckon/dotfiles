7af8929cf06c91534ee688f9c0817027aa632bfb
eckon <niklas.meyer95@hotmail.de>
2020-10-28 18:57:12 +0100

    Add script to setup all singularity dependencies

---
+++ custom-scripts/setup-singularity.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+session="Singularity"
+pathApi="/home/eckon/Development/singularity2-api"
+pathFrontend="/home/eckon/Development/singularity2-frontend"
+
+# only run tmux script when the session is not already created
+tmux has-session -t $session 2>/dev/null
+
+if [ $? != 0 ]; then
+  # handle backend api project view
+  tmux new-session -s $session -d -c $pathApi
+  tmux rename-window -t $session:1 'Api'
+  tmux send-key -t $session:1 'vv' C-m
+  tmux split-window -t $session:1 -p 20 -v -c $pathApi
+  tmux send-key -t $session:1 'npm run start:dev' C-m
+  tmux split-window -t $session:1 -h -c $pathApi
+  tmux select-pane -t $session:1 -U
+
+  # handle frontend project view
+  tmux new-window -t $session:2 -n 'Frontend' -c $pathFrontend
+  tmux send-key -t $session:2 'vv' C-m
+  tmux split-window -t $session:2 -p 20 -v -c $pathFrontend
+  tmux send-key -t $session:2 'npm start' C-m
+  tmux split-window -t $session:2 -h -c $pathFrontend
+  tmux select-pane -t $session:2 -U
+
+  # handle backend structure view
+  tmux new-window -t $session:3 -n 'Api-Docker' -c $pathApi
+  tmux send-key -t $session:3 'docker-compose up' C-m
+
+  tmux select-window -t $session:1
+fi
+
+tmux attach-session -t $session
