7700c614a934f5a9c771cfeafbc46fc0fb109001
eckon <niklas.meyer95@hotmail.de>
2020-10-29 11:33:50 +0100

    Do not show additional help
    Meaning things that are not awesome. Example vim/tmux should not be
    shown.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -16,7 +16,7 @@ local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
-require("awful.hotkeys_popup.keys")
+-- require("awful.hotkeys_popup.keys")
 
 -- Load Debian menu entries
 local debian = require("debian.menu")
