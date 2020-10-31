ff00b721266be3cf2593b5eb3b0a38f3a64f317a
eckon <niklas.meyer95@hotmail.de>
2020-10-31 22:29:57 +0100

    Select favorite layouts

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -73,22 +73,9 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    -- awful.layout.sujt.floating,
-    -- awful.layout.suit.tile,
-    -- awful.layout.suit.tile.left,
-    -- awful.layout.suit.tile.bottom,
-    -- awful.layout.suit.tile.top,
-    -- awful.layout.suit.fair,
-    -- awful.layout.suit.fair.horizontal,
-    -- awful.layout.suit.spiral,
+    awful.layout.suit.tile,
+    awful.layout.suit.magnifier,
     awful.layout.suit.spiral.dwindle,
-    -- awful.layout.suit.max,
-    -- awful.layout.suit.max.fullscreen,
-    -- awful.layout.suit.magnifier,
-    -- awful.layout.suit.corner.nw,
-    -- awful.layout.suit.corner.ne,
-    -- awful.layout.suit.corner.sw,
-    -- awful.layout.suit.corner.se,
 }
 -- }}}
 
@@ -258,8 +245,7 @@ awful.screen.connect_for_each_screen(function(s)
                 step_spacing = 0,
                 width = 25,
             }),
-            -- disable layout symbol in wibar
-            -- s.mylayoutbox,
+            s.mylayoutbox,
         },
     }
 end)
