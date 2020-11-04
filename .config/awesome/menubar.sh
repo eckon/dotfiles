d50b26605016f7f39b2eb932ac2cbd242c72c326
eckon <niklas.meyer95@hotmail.de>
2020-11-04 08:26:05 +0100

    Fix code styling of menu bar and add status

--- .config/awesome/menubar.sh
+++ .config/awesome/menubar.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 
-mem(){
+mem() {
   mem="$(free -h | awk '/^Mem:/ {print $3 "/" $2}')"
   echo "Mem: $mem"
 }
 
-vol(){
+vol() {
   vol="$(amixer -D pulse get Master | awk -F'[][]' 'END{print $4":"$2}')"
   echo "Vol: $vol"
 }
 
 bat() {
   battery="$(cat /sys/class/power_supply/BAT0/capacity)"
-  echo "Bat: $battery%"
+  status="$(cat /sys/class/power_supply/BAT0/status)"
+
+  if [ "$status" = "Discharging" ]; then
+    chargeSymbol="(-)"
+  else
+    chargeSymbol="(+)"
+  fi
+
+  echo "Bat$chargeSymbol: $battery%"
 }
 
 echo "| $(mem) | $(vol) | $(bat) |"
