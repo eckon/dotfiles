6ed6dcae13b6433843c1a3eb66748f658fae66e3
eckon <niklas.meyer95@hotmail.de>
2020-11-20 19:32:43 +0100

    Wait until polybar has stopped when launching it

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 killall -q polybar
-polybar bar >/dev/null 2>&1 & disown
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar
+polybar bar >/dev/null 2>&1 &
