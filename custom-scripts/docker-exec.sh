6fe3b7080f2f8c0687f1ac1315842f1b21df8544
eckon <niklas.meyer95@hotmail.de>
2021-01-19 15:53:14 +0100

    Optimize the docker execute script
    Now it will not have the header inside of the fzf result.
    The header will be shown as a fzf header. Additionally fzf will reverse
    itself, meaning now it will go from top to bottom, also showing the
    header on top instead of the bottom.

--- custom-scripts/docker-exec.sh
+++ custom-scripts/docker-exec.sh
@@ -2,11 +2,19 @@
 
 # script to quickly run the docker exec command with bash/sh on chosen container
 
+# add the header to fzf, so we add it here (only the first line) and
+# delete the header in the later docker call (everthing except the first line)
+header=$(
+  docker ps --format="table {{.Names}}\t{{.Ports}}\t{{.Image}}\t{{.ID}}" | \
+    awk 'NR == 1'
+)
+
 # get all running docker container and select one with fzf
 container=$(
   {
-    docker ps --format="table {{.Names}}\t{{.Ports}}\t{{.Image}}\t{{.ID}}"
-  } | fzf
+    docker ps --format="table {{.Names}}\t{{.Ports}}\t{{.Image}}\t{{.ID}}" | \
+      awk 'NR > 1'
+  } | fzf --reverse --header="$header"
 )
 
 # get the last field (container id) out of the selected container
