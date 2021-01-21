b1054ec88a34400f5eae3409af5e46459d7b79a3
eckon <niklas.meyer95@hotmail.de>
2021-01-21 10:29:16 +0100

    Update docker-execute script
    Use a variable for the docker output to not call it multiple times.
    Add more responses to the user so they know what happens.
    Add check if there is even one running image before continuing.
    Update the logic to docker execute the id.

--- custom-scripts/docker-exec.sh
+++ custom-scripts/docker-exec.sh
@@ -1,19 +1,31 @@
 #!/usr/bin/env bash
 
+##########
 # script to quickly run the docker exec command with bash/sh on chosen container
+##########
+
+# evaluate docker output once to not call it multiple times
+data=$(
+  docker ps --format="table {{.Names}}\t{{.Ports}}\t{{.Image}}\t{{.ID}}"
+)
+
+# check if theres at least one running docker (2 lines, first is the header)
+running=$(echo "$data" | wc -l)
+if [[ $running -le 1 ]]; then
+  echo "No images are running, exiting script"
+  exit
+fi
 
 # add the header to fzf, so we add it here (only the first line) and
 # delete the header in the later docker call (everthing except the first line)
 header=$(
-  docker ps --format="table {{.Names}}\t{{.Ports}}\t{{.Image}}\t{{.ID}}" | \
-    awk 'NR == 1'
+  echo "$data" | awk 'NR == 1'
 )
 
 # get all running docker container and select one with fzf
 container=$(
   {
-    docker ps --format="table {{.Names}}\t{{.Ports}}\t{{.Image}}\t{{.ID}}" | \
-      awk 'NR > 1'
+    echo "$data" | awk 'NR > 1'
   } | fzf --reverse --header="$header"
 )
 
@@ -22,11 +34,16 @@ id=$(
   echo "$container" | awk '{print $NF}'
 )
 
-if [[ $id != "" ]]; then
-  docker exec -it "$id" bash
+if [[ $id == "" ]]; then
+  echo "No image was selected, exiting script"
+  exit
+fi
 
-  # if bash did not work try sh as tty
-  if [[ $? != 0 ]]; then
-    docker exec -it "$id" sh
-  fi
+# try executing given container id with bash (if error continue)
+docker exec -it "$id" bash
+
+# if bash did not work try sh as tty
+if [[ $? != 0 ]]; then
+  echo "Bash not found, try sh"
+  docker exec -it "$id" sh
 fi
