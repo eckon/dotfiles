c8a844dffaf964b0881deaea9626a28b2f2822b9
eckon <niklas.meyer95@hotmail.de>
2021-02-02 12:51:15 +0100

    Add script to automatically add symlinks

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+echo "[!] Start: Linking of dotfiles"
+
+dotfilesDirectory=$(pwd)
+
+function symlink {
+  destination="${HOME}/${1}"
+  # add new line for better formatting
+  echo ""
+
+  if [ -h "${destination}" ]; then
+    echo "[-] Removing existing symlink: \"${destination}\""
+    rm "${destination}"
+
+  elif [ -f "${destination}" ]; then
+    echo "[~] Moving existing file: \"${destination}\""
+    mv "${destination}" "${destination}.old"
+
+  elif [ -d "${destination}" ]; then
+    echo "[~] Moving existing dir: \"${destination}\""
+    mv "${destination}" "${destination}.old"
+  fi
+
+  base="${dotfilesDirectory}/${1}"
+  echo "[+] Creating new symlink: \"${1}\""
+  echo "[!] ${base} -> ${destination}"
+  ln -s "${base}" "${destination}"
+}
+
+symlink .config/starship.toml
+symlink .config/nvim
+symlink .config/rofi
+symlink .zshrc
