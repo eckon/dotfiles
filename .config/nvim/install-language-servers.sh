ded30ec6b86e1adaf1d2b4990c2eb7715dcec733
eckon <niklas.meyer95@hotmail.de>
2021-03-19 22:39:40 +0100

    Update needed language server packages

--- .config/nvim/install-language-servers.sh
+++ .config/nvim/install-language-servers.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 
-npm install -g @angular/language-server
+npm install -g typescript
+
+npm install -g @angular/language-server @angular/language-service
 npm install -g intelephense
-npm install -g typescript typescript-language-server
+npm install -g typescript-language-server
 npm install -g vim-language-server
 npm install -g vls
 npm install -g vscode-html-languageserver-bin
