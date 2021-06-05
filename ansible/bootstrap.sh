038eb994f05b38e49afa6b8109f669411afba716
eckon <niklas.meyer95@hotmail.de>
2021-06-05 18:29:02 +0200

    Enclose bash statement to prevent word splitting

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v ansible)" ]; then
   exit 1
 fi
 
-SCRIPT_PATH=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+SCRIPT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 INVENTORY="$SCRIPT_PATH/hosts"
 PLAYBOOK="$SCRIPT_PATH/roles.yml"
 
