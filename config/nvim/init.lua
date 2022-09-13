2d38da2fcc02d3a4e3acdd447a4ff5f93ee227f1
eckon <niklas.meyer95@hotmail.de>
2022-09-13 17:28:49 +0200

    Order sourcing of lua parts correctly

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,7 +1,8 @@
+-- order matters, otherwise keymaps dont have correct leader etc.
+require('eckon.basic.settings')
+require('eckon.basic.keymaps')
 require('eckon.basic.autocommands')
 require('eckon.basic.commands')
-require('eckon.basic.keymaps')
-require('eckon.basic.settings')
 
 require('eckon.custom')
 require('eckon.plugins')
