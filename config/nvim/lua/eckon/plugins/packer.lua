2976e7a73aa3d3426fad50af0dc4127d7230df1d
eckon <niklas.meyer95@hotmail.de>
2022-10-11 11:54:10 +0200

    Tryout cmp completion for commandline mode

--- config/nvim/lua/eckon/plugins/packer.lua
+++ config/nvim/lua/eckon/plugins/packer.lua
@@ -27,6 +27,7 @@ require('packer').startup(function(use)
   use({
     'hrsh7th/nvim-cmp',
     'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-cmdline',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-vsnip',
