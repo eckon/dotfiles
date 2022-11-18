37cd89b70149c2d1bd62090630cc800ac4d655e1
eckon <niklas.meyer95@hotmail.de>
2022-11-18 16:44:31 +0100

    Ignore third part for lua lsp
    This would otherwise always ask me about creating a luarc.

--- config/nvim/lua/eckon/plugins/lsp.lua
+++ config/nvim/lua/eckon/plugins/lsp.lua
@@ -76,7 +76,10 @@ require('mason-lspconfig').setup_handlers({
         Lua = {
           runtime = { version = 'LuaJIT' },
           diagnostics = { globals = { 'vim' } },
-          workspace = { library = vim.api.nvim_get_runtime_file('', true) },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file('', true),
+            checkThirdParty = false,
+          },
           telemetry = { enable = false },
         },
       },
