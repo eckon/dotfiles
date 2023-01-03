3e70b5c51c28908fb02a540cd4fee07a5aa946de
eckon <niklas.meyer95@hotmail.de>
2023-01-03 16:48:03 +0100

    Add which key and todo comment plugin

--- config/nvim/lua/eckon/plugins/init.lua
+++ config/nvim/lua/eckon/plugins/init.lua
@@ -3,9 +3,29 @@ return {
   { "tpope/vim-sleuth", event = "VeryLazy" },
   { "kylechui/nvim-surround", event = "VeryLazy", config = function() require("nvim-surround").setup() end },
   { "numToStr/Comment.nvim", event = "VeryLazy", config = function() require("Comment").setup() end },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    config = function()
+      local key = require("which-key")
+      key.setup({ plugins = { spelling = { enabled = true } } })
+      key.register({
+        f = { name = "find" },
+        g = { name = "git" },
+        l = { name = "lsp" },
+      }, { prefix = "<Leader>" })
+    end,
+  },
 
   -- Styling/Appearance/Special
   { "tmux-plugins/vim-tmux-focus-events", event = "VeryLazy" },
+  {
+    "folke/todo-comments.nvim",
+    cmd = { "TodoQuickFix", "TodoTelescope" },
+    event = "BufReadPost",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = function() require("todo-comments").setup({ signs = false }) end,
+  },
   {
     "navarasu/onedark.nvim",
     config = function()
