913ede1375a508f36564e8f51dade3118066db87
eckon <niklas.meyer95@hotmail.de>
2023-01-04 20:51:54 +0100

    Try out harpoon

---
+++ config/nvim/lua/eckon/plugins/harpoon.lua
@@ -0,0 +1,26 @@
+local nnoremap = require("eckon.utils").nnoremap
+
+local M = {
+  "ThePrimeagen/harpoon",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function() require("harpoon").setup() end,
+  init = function()
+    -- TODO: if using, dont forget to add leader h as which key desciption
+    nnoremap("<Leader>ha", function() require("harpoon.mark").add_file() end, { desc = "Add file to harpoon" })
+    nnoremap(
+      "<Leader>hh",
+      function() require("harpoon.ui").toggle_quick_menu() end,
+      { desc = "Open harpoon quick menu" }
+    )
+
+    for i = 1, 4, 1 do
+      nnoremap(
+        "<M-" .. i .. ">",
+        function() require("harpoon.ui").nav_file(i) end,
+        { desc = "Jump to harpoon file " .. i }
+      )
+    end
+  end,
+}
+
+return M
