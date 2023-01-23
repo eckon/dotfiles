b0c79d92ca122c5bc407fe553a29e50e5b115104
eckon <niklas.meyer95@hotmail.de>
2023-01-23 23:05:34 +0100

    Merge neotree and nvimtree into a filetree plugin file

---
+++ config/nvim/lua/eckon/plugins/filetree.lua
@@ -0,0 +1,62 @@
+local nnoremap = require("eckon.utils").nnoremap
+
+local M = {
+  {
+    "nvim-tree/nvim-tree.lua",
+    cmd = "NvimTreeFindFileToggle",
+    enabled = false,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup({
+        actions = { open_file = { quit_on_open = true } },
+        git = { ignore = false },
+        renderer = {
+          add_trailing = true,
+          group_empty = true,
+        },
+        view = {
+          adaptive_size = true,
+          hide_root_folder = true,
+          relativenumber = true,
+        },
+      })
+    end,
+    init = function() nnoremap("<Leader>.", "<CMD>NvimTreeFindFileToggle<CR>", { desc = "Toggle tree view" }) end,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    cmd = "Neotree",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("neo-tree").setup({
+        -- never want to update the root of my repo (which `bs` and `.` does)
+        window = {
+          mappings = {
+            ["<bs>"] = "none",
+            ["."] = "none",
+          },
+          auto_expand_width = true,
+        },
+        event_handlers = {
+          {
+            event = "neo_tree_buffer_enter",
+            handler = function()
+              vim.opt.relativenumber = true
+              vim.opt.number = true
+            end,
+          },
+        },
+      })
+    end,
+    init = function()
+      vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+      nnoremap("<Leader>ft", "<CMD>Neotree toggle reveal_force_cwd<CR>", { desc = "Toggle file tree" })
+    end,
+  },
+}
+
+return M
