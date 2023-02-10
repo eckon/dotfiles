a4974fa30eb2bb2a181cff62213f037f74ec1573
eckon <niklas.meyer95@hotmail.de>
2023-02-10 16:10:47 +0100

    Try out copilot as ai assistant

--- config/nvim/lua/eckon/plugins/_experimental.lua
+++ config/nvim/lua/eckon/plugins/_experimental.lua
@@ -2,8 +2,37 @@ local nnoremap = require("eckon.utils").nnoremap
 
 local M = {
   {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      -- it also has a cmp plugin, but that one does not really complete always as good as the dafault plugin
+      require("copilot").setup({
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          -- emulate the same as normal completion just with `alt` key instead
+          keymap = {
+            accept = "<M-CR>",
+            next = "<M-n>",
+            prev = "<M-p>",
+          },
+        },
+        -- only allow for specific files for now
+        filetypes = {
+          lua = true,
+          rust = true,
+          typescript = true,
+          ["*"] = false,
+        },
+      })
+    end,
+  },
+  {
     "ThePrimeagen/harpoon",
     dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = true,
     config = function() require("harpoon").setup() end,
     init = function()
       nnoremap("<Leader>ha", function() require("harpoon.mark").add_file() end, { desc = "Add file to harpoon" })
