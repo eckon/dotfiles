2de1612721564263527003e8c70fec7f8a5bb75e
eckon <niklas.meyer95@hotmail.de>
2023-05-03 17:08:10 +0200

    Speed up indentscope animation

--- config/nvim/lua/eckon/plugins/mini.lua
+++ config/nvim/lua/eckon/plugins/mini.lua
@@ -5,7 +5,11 @@ local M = {
   config = function()
     require("mini.comment").setup()
     -- this also has ii/ai text objects
-    require("mini.indentscope").setup()
+    require("mini.indentscope").setup({
+      draw = {
+        animation = require("mini.indentscope").gen_animation.linear({ duration = 5 }),
+      },
+    })
     require("mini.pairs").setup()
   end,
 }
