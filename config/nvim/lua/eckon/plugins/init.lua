77c5a3073f2965406fe73ee399b9e13919df1c52
eckon <niklas.meyer95@hotmail.de>
2023-05-09 18:17:18 +0200

    Move mini plugins into the general init file

--- config/nvim/lua/eckon/plugins/init.lua
+++ config/nvim/lua/eckon/plugins/init.lua
@@ -8,6 +8,20 @@ return {
       require("nvim-surround").setup()
     end,
   },
+  {
+    "echasnovski/mini.nvim",
+    event = "BufReadPre",
+    version = false,
+    config = function()
+      require("mini.comment").setup()
+      -- this also has ii/ai text objects
+      require("mini.indentscope").setup({
+        draw = {
+          animation = require("mini.indentscope").gen_animation.linear({ duration = 5 }),
+        },
+      })
+    end,
+  },
 
   -- Styling/Appearance/Special
   { "tmux-plugins/vim-tmux-focus-events" },
