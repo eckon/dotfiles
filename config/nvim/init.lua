cc6a1bea1d8e218f886671e64035ab6d7ef86cfa
eckon <niklas.meyer95@hotmail.de>
2023-05-16 16:46:13 +0200

    Rename lazypath to snake case

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,17 +1,17 @@
 require("eckon.basic")
 
 -- package/plugins management
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazy_path) then
   vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable",
-    lazypath,
+    lazy_path,
   })
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(lazy_path)
 
 require("lazy").setup("eckon.plugins")
