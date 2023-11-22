6dd4196e2006865a891d263d7b3fb725a53d11a8
eckon <niklas.meyer95@hotmail.de>
2023-11-22 18:25:27 +0100

    Try out treesitter context
    But disable it on bigger files, similar to treesitter itself
    This was the main pain point, slowness in big files
    
    I might delete it later, but want to try

--- config/nvim/lua/eckon/plugins/treesitter.lua
+++ config/nvim/lua/eckon/plugins/treesitter.lua
@@ -5,35 +5,53 @@ if require("eckon.utils").run_minimal() then
 end
 
 local M = {
-  "nvim-treesitter/nvim-treesitter",
-  event = "BufReadPost",
-  build = ":TSUpdate",
-  config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = "all",
-      highlight = {
-        enable = true,
-        disable = function(_, bufnr)
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = "BufReadPost",
+    build = ":TSUpdate",
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = "all",
+        highlight = {
+          enable = true,
+          disable = function(_, bufnr)
+            if vim.api.nvim_buf_line_count(bufnr) < 10000 then
+              return false
+            end
+
+            vim.notify("Big File: Stopped Treesitter")
+            return true
+          end,
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<CR>",
+            node_incremental = "<CR>",
+            scope_incremental = "<S-CR>",
+            node_decremental = "<BS>",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "BufReadPost",
+    config = function()
+      require("treesitter-context").setup({
+        on_attach = function(bufnr)
           if vim.api.nvim_buf_line_count(bufnr) < 10000 then
-            return false
+            return true
           end
 
-          vim.notify("Stopped Treesitter (file too big)")
-          return true
+          vim.notify("Big File: Stopped Treesitter-Context")
+          return false
         end,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<CR>",
-          node_incremental = "<CR>",
-          scope_incremental = "<S-CR>",
-          node_decremental = "<BS>",
-        },
-      },
-    })
-  end,
+      })
+    end,
+  },
 }
 
 return M
