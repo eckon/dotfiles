8c7a1995fe9645e78bc29a19bd553672ffeb0c7f
eckon <niklas.meyer95@hotmail.de>
2024-01-29 10:06:43 +0100

    Align vim color to updated treesitter nodes
    Mainly as markdown was updated and the color repo is not yet updated
    so we overwrite some queries

--- config/nvim/lua/eckon/plugins/init.lua
+++ config/nvim/lua/eckon/plugins/init.lua
@@ -60,6 +60,34 @@ return {
   {
     "rebelot/kanagawa.nvim",
     config = function()
+      require("kanagawa").setup({
+        overrides = function()
+          return {
+            -- update kanagawa to handle new treesitter highlight captures
+            -- change color theme in case this is not being added in the future
+            ["@string.regexp"] = { link = "@string.regex" },
+            ["@variable.parameter"] = { link = "@parameter" },
+            ["@exception"] = { link = "@exception" },
+            ["@string.special.symbol"] = { link = "@symbol" },
+            ["@markup.strong"] = { link = "@text.strong" },
+            ["@markup.italic"] = { link = "@text.emphasis" },
+            ["@markup.heading"] = { link = "@text.title" },
+            ["@markup.raw"] = { link = "@text.literal" },
+            ["@markup.quote"] = { link = "@text.quote" },
+            ["@markup.math"] = { link = "@text.math" },
+            ["@markup.environment"] = { link = "@text.environment" },
+            ["@markup.environment.name"] = { link = "@text.environment.name" },
+            ["@markup.link.url"] = { link = "Special" },
+            ["@markup.link.label"] = { link = "Identifier" },
+            ["@comment.note"] = { link = "@text.note" },
+            ["@comment.warning"] = { link = "@text.warning" },
+            ["@comment.danger"] = { link = "@text.danger" },
+            ["@diff.plus"] = { link = "@text.diff.add" },
+            ["@diff.minus"] = { link = "@text.diff.delete" },
+          }
+        end,
+      })
+
       vim.cmd("colorscheme kanagawa")
     end,
   },
