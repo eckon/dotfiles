ee36a81da31581e05b730e2803d646b7aea9ea31
eckon <niklas.meyer95@hotmail.de>
2024-06-15 19:24:26 +0200

    Try out copilot alternative

--- config/nvim/lua/eckon/plugins/copilot.lua
+++ config/nvim/lua/eckon/plugins/copilot.lua
@@ -2,20 +2,32 @@ if require("eckon.utils").run_minimal() then
   return {}
 end
 
+local use_supermaven = true
+
 local M = {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        debounce = 75,
-        keymap = { accept = "<M-CR>", next = "<M-n>", prev = "<M-p>" },
-      },
-    })
-  end,
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    enabled = not use_supermaven,
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = { accept = "<M-CR>", next = "<M-n>", prev = "<M-p>" },
+        },
+      })
+    end,
+  },
+  {
+    "supermaven-inc/supermaven-nvim",
+    enabled = use_supermaven,
+    config = function()
+      require("supermaven-nvim").setup({})
+    end,
+  },
 }
 
 return M
