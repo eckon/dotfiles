5102f8e8058d0150258bfeba01d41895e77a2870
eckon <niklas.meyer95@hotmail.de>
2024-08-15 15:00:28 +0200

    Add k9s pretty print plugin

--- bootstrap/symlink.sh
+++ bootstrap/symlink.sh
@@ -16,6 +16,7 @@ CONFIG_PATHS=(
   ["config/helix/config.toml"]=".config/helix/config.toml"
   ["config/jetbrains/ideavimrc"]=".config/ideavim/ideavimrc"
   ["config/k9s/config.yaml"]=".config/k9s/config.yaml"
+  ["config/k9s/plugins.yaml"]=".config/k9s/plugins.yaml"
   ["config/kitty/kitty.conf"]=".config/kitty/kitty.conf"
   ["config/lazygit/config.yml"]=".config/lazygit/config.yml"
   ["config/nvim"]=".config/nvim"
