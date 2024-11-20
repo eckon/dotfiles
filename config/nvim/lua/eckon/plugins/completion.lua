0a811dac434ada0187e0ac7b56caf87288e2c87c
eckon <niklas.meyer95@hotmail.de>
2024-11-20 19:15:21 +0100

    Remove neovim blink completion plugin
    As I do not use it, and it has missing features (command/search
    completion), I will most likely not switch anytime soon.
    Additionally there autocompletion order was weird when it comes to
    pathing and other things, that just work with my cmp setup.
    
    I might still switch in the future, but then I want to switch completly
    and not have a half/half implementation. For that reason I remove it
    here, to not keep old deprecated configurations - I will need to relearn
    the plugin when its in a more mature state.

--- config/nvim/lua/eckon/plugins/completion.lua
+++ config/nvim/lua/eckon/plugins/completion.lua
@@ -1,116 +1,81 @@
--- fallback to cmp as blink has no auto import and has still some completion issues
-local use_blink = false
-
 return {
-  {
-    "saghen/blink.cmp",
-    enabled = use_blink,
-    lazy = false,
-    dependencies = "rafamadriz/friendly-snippets",
-    version = "v0.*",
-    config = function()
-      ---@diagnostic disable: missing-fields
-      require("blink.cmp").setup({
-        accept = { auto_brackets = { enabled = true } },
-        trigger = { signature_help = { enabled = true } },
-        keymap = {
-          accept = "<CR>",
-          select_prev = "<C-p>",
-          select_next = "<C-n>",
-          scroll_documentation_up = "<C-u>",
-          scroll_documentation_down = "<C-d>",
-        },
-        windows = {
-          autocomplete = { border = "single", selection = "manual" },
-          documentation = { border = "single", auto_show = true },
-        },
-      })
-    end,
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-path",
   },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-cmdline",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-path",
-    },
-    config = function()
-      local cmp = require("cmp")
+  config = function()
+    local cmp = require("cmp")
 
-      -- disabled completion in bigfiles
-      cmp.setup.filetype({ "bigfile" }, { enabled = false })
+    -- disabled completion in bigfiles
+    cmp.setup.filetype({ "bigfile" }, { enabled = false })
 
-      cmp.setup.cmdline({ "/", "?" }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          {
-            name = "buffer",
-            option = {
-              get_bufnrs = function()
-                -- ignore if current buffer is a bigfile
-                local ft = vim.api.nvim_get_option_value("filetype", {})
-                if ft == "bigfile" then
-                  return {}
-                end
+    cmp.setup.cmdline({ "/", "?" }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        {
+          name = "buffer",
+          option = {
+            get_bufnrs = function()
+              -- ignore if current buffer is a bigfile
+              local ft = vim.api.nvim_get_option_value("filetype", {})
+              if ft == "bigfile" then
+                return {}
+              end
 
-                local buf = vim.api.nvim_get_current_buf()
-                return { buf }
-              end,
-            },
+              local buf = vim.api.nvim_get_current_buf()
+              return { buf }
+            end,
           },
         },
-      })
+      },
+    })
 
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
-      })
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
+    })
 
-      -- NOTE: these parts are here even for blink, as I like the completion in filter/commandline
-      --       but blink does not support it yet
-      if use_blink then
-        return
-      end
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          vim.snippet.expand(args.body)
+        end,
+      },
 
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            vim.snippet.expand(args.body)
-          end,
-        },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+      }),
 
-        mapping = cmp.mapping.preset.insert({
-          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-d>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = false }),
-        }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+        { name = "path" },
+      }),
 
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "buffer" },
-          { name = "path" },
-        }),
-
-        formatting = {
-          format = function(_, vim_item)
-            local label = vim_item.abbr
-            if label == nil then
-              return vim_item
-            end
-
-            local truncated_label = vim.fn.strcharpart(label, 0, 50)
-            -- shorten really long completion labels
-            if truncated_label ~= label then
-              vim_item.abbr = truncated_label .. "…"
-            end
-
+      ---@diagnostic disable-next-line: missing-fields
+      formatting = {
+        format = function(_, vim_item)
+          local label = vim_item.abbr
+          if label == nil then
             return vim_item
-          end,
-        },
-      })
-    end,
-  },
+          end
+
+          local truncated_label = vim.fn.strcharpart(label, 0, 50)
+          -- shorten really long completion labels
+          if truncated_label ~= label then
+            vim_item.abbr = truncated_label .. "…"
+          end
+
+          return vim_item
+        end,
+      },
+    })
+  end,
 }
