ee14df9483f67c4e391c3fecb9b625c268f75a60
eckon <niklas.meyer95@hotmail.de>
2025-08-13 20:29:02 +0200

    Add opencode notification plugin
    This allows me to keep the agent in the background and still get
    notified when its done

---
+++ config/opencode/plugin/notification.ts
@@ -0,0 +1,25 @@
+import { Plugin } from "@opencode-ai/plugin";
+
+// to use just open the url; leaking topic as nothing critical will be sent
+const url = "https://ntfy.sh/eckon_uDoe1CGbV7uRc89e";
+
+export const NotificationPlugin: Plugin = async ({ $ }) => {
+  const notify = async (message: string) =>
+    await $`curl --silent --output /dev/null -d ${message} ${url}`;
+
+  return {
+    // notify user, when AI is waiting for input/permission to enable leaving it in the background
+    event: async ({ event }) => {
+      switch (event.type) {
+        case "session.idle":
+          await notify("AI is done, waiting for new input");
+          break;
+        case "permission.updated":
+          await notify(
+            `AI is requesting permission "${event.properties.type}" > "${event.properties.metadata.command}"`,
+          );
+          break;
+      }
+    },
+  };
+};
