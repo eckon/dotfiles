fbac98cdf5cc81a8d41f53bf396f6ed1bfc274c4
eckon <niklas.meyer95@hotmail.de>
2025-09-14 14:48:57 +0200

    Remove neovim flash plugin
    I never use it

--- config/nvim/lua/eckon/plugins/others.lua
+++ config/nvim/lua/eckon/plugins/others.lua
@@ -1,5 +1,4 @@
 return {
-  -- General
   {
     "kylechui/nvim-surround",
     event = "BufReadPre",
@@ -8,30 +7,11 @@ return {
     end,
   },
   {
-    "folke/flash.nvim",
-    event = "BufReadPre",
-    config = function()
-      require("flash").setup({
-        modes = {
-          search = { enabled = false },
-          char = { enabled = false },
-        },
-      })
-    end,
-    init = function()
-      require("eckon.utils").bind_map({ "n", "o", "x" })("H", function()
-        require("flash").jump()
-      end, { desc = "Flash: Jump" })
-    end,
-  },
-  {
     "stevearc/quicker.nvim",
     config = function()
       require("quicker").setup()
     end,
   },
-
-  -- Styling/Appearance/Special
   {
     "MeanderingProgrammer/markdown.nvim",
     config = function()
