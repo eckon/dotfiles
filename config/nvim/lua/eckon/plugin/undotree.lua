a7b026cf99a2848a648d524298f3664f48b13cfb
eckon <niklas.meyer95@hotmail.de>
2025-10-09 10:49:18 +0200

    Simplify undotree title
    The bug was fixed and just a string can be returned now

--- config/nvim/lua/eckon/plugin/undotree.lua
+++ config/nvim/lua/eckon/plugin/undotree.lua
@@ -8,12 +8,7 @@ cc.add("Undotree", {
     require("undotree").open({
       -- increase default width and add filetype to add keybindings via `ftplugin`
       command = "50vnew | setlocal ft=undotree",
-      title = function(buf)
-        local file_path = vim.api.nvim_buf_get_name(buf)
-        local file_name = vim.fn.fnamemodify(file_path, ":t")
-
-        return "Undotree: " .. file_name
-      end,
+      title = "Undotree",
     })
   end,
 })
