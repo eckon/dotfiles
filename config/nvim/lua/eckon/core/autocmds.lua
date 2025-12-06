48fdfc36eb69b1667f533509d6f082f55f11535f
eckon <niklas.meyer95@hotmail.de>
2025-12-06 22:18:05 +0100

    Add a yankring to neovim

--- config/nvim/lua/eckon/core/autocmds.lua
+++ config/nvim/lua/eckon/core/autocmds.lua
@@ -40,3 +40,14 @@ autocmd("BufWritePre", {
   end,
   group = augroup,
 })
+
+autocmd("TextYankPost", {
+  desc = "Yank-ring: store yanked text in registers 1-9",
+  callback = function()
+    if vim.v.event.operator == "y" then
+      for i = 9, 1, -1 do -- Shift all numbered registers.
+        vim.fn.setreg(tostring(i), vim.fn.getreg(tostring(i - 1)))
+      end
+    end
+  end,
+})
