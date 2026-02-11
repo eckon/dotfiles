d612df275e9c808267286efdaf950bf904174e8a
eckon <niklas.meyer95@hotmail.de>
2026-02-11 19:55:03 +0100

    Use no-op for installation script comments

--- bootstrap/packages/install-packages-apt.sh
+++ bootstrap/packages/install-packages-apt.sh
@@ -9,14 +9,14 @@ sudo apt install -y \
   build-essential \
   cmake \
   pkg-config \
-  libevent-dev `# event notification library` \
-  libfuse2 `# filesystem in userspace` \
-  libncurses5-dev `# terminal UI library` \
-  libncursesw5-dev `# ncurses with wide character support` \
-  entr `# run commands when files change` \
-  moreutils `# additional Unix utilities` \
+  libevent-dev         $(: "event notification library") \
+  libfuse2             $(: "filesystem in userspace") \
+  libncurses5-dev      $(: "terminal UI library") \
+  libncursesw5-dev     $(: "ncurses with wide character support") \
+  entr                 $(: "run commands when files change") \
+  moreutils            $(: "additional Unix utilities") \
   unzip \
-  watchman `# file watching service` \
+  watchman             $(: "file watching service") \
   wget \
-  xclip `# clipboard utility for X11` \
-  yad `# display dialogs from shell scripts`
+  xclip                $(: "clipboard utility for X11") \
+  yad                  $(: "display dialogs from shell scripts")
