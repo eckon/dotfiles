3fe4f5d575e027c8926f73bbb68b61eea8cd15dd
eckon <niklas.meyer95@hotmail.de>
2026-03-27 16:55:21 +0100

    Show what package manager is being used in update script

--- bootstrap/update-packages.sh
+++ bootstrap/update-packages.sh
@@ -7,6 +7,9 @@ set -euo pipefail
 ######################################################################
 
 if command -v "yay" &> /dev/null; then
+  echo ""
+  echo "########## yay ##########"
+  echo ""
   yay -Syu --noconfirm
   yay -Yc --noconfirm
   yay -Sc --noconfirm
@@ -14,27 +17,42 @@ if command -v "yay" &> /dev/null; then
 fi
 
 if command -v "apt" &> /dev/null; then
+  echo ""
+  echo "########## apt ##########"
+  echo ""
   sudo apt update
   sudo apt upgrade -y
   sudo apt autoremove -y
 fi
 
 if command -v "dnf" &> /dev/null; then
+  echo ""
+  echo "########## dnf ##########"
+  echo ""
   sudo dnf upgrade -y
   sudo dnf autoremove -y
 fi
 
 if command -v "brew" &> /dev/null; then
+  echo ""
+  echo "########## brew ##########"
+  echo ""
   brew update
   brew upgrade
   brew cleanup
 fi
 
 if command -v "flatpak" &> /dev/null; then
+  echo ""
+  echo "########## flatpak ##########"
+  echo ""
   flatpak update -y
   flatpak uninstall --unused -y
 fi
 
 if command -v "snap" &> /dev/null; then
+  echo ""
+  echo "########## snap ##########"
+  echo ""
   sudo snap refresh
 fi
