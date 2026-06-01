fd6e9473b49f2102d102c191e8015cebbdeeb939
eckon <niklas.meyer95@hotmail.de>
2026-06-01 15:44:47 +0200

    Ignore firenvim setup

--- config/nvim/lua/eckon/plugin/firenvim.lua
+++ config/nvim/lua/eckon/plugin/firenvim.lua
@@ -1,7 +1,7 @@
 vim.pack.add({ "https://github.com/glacambre/firenvim" })
 
 -- for now always trigger it, in the future handle it via hooks, see https://echasnovski.com/blog/2026-03-13-a-guide-to-vim-pack#hooks
-vim.cmd(":call firenvim#install(0)")
+-- vim.cmd(":call firenvim#install(0)")
 
 if vim.g.started_by_firenvim == true then
   vim.o.laststatus = 0
