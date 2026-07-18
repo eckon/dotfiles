e2bff382fff6190f585aa78495c735026d6d71f4
eckon <niklas.meyer95@hotmail.de>
2026-07-18 23:09:37 +0200

    Start markoff with the context selector

--- scripts/todo.sh
+++ scripts/todo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# start custom markoff plugin agenda view
-nvim -c "lua require('markoff').open_list()"
+# start custom markoff plugin in the context question view, interrupting it or closing the float will close neovim
+nvim -c "autocmd UIEnter * ++once lua vim.schedule(function() require('markoff').open_list({ ask_context = true }) end)"
