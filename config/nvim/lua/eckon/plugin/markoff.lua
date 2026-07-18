61608bdc5c6bbebae9fa93644a4e24feabcdf969
eckon <niklas.meyer95@hotmail.de>
2026-07-18 18:36:22 +0200

    Align markoff

--- config/nvim/lua/eckon/plugin/markoff.lua
+++ config/nvim/lua/eckon/plugin/markoff.lua
@@ -4,30 +4,22 @@ vim.opt.runtimepath:append(vim.fn.expand("~/Development/personal/markoff.nvim"))
 
 local cc = require("eckon.helper.custom-command").custom_command
 
-require("markoff").setup({
+local markoff = require("markoff")
+
+markoff.setup({
   todo_file = vim.fn.expand("~/Documents/notes/todo.md"),
-  quit_on_last_buffer = true,
 
   on_attach = function(buf, api)
     local nmap = require("eckon.helper.utils").bind_map("n", { buffer = buf })
 
-    nmap("td", api.mark_done, { desc = "Markoff: mark done" })
-    nmap("tx", api.mark_cancelled, { desc = "Markoff: mark cancelled" })
-    nmap("tp", api.set_priority, { desc = "Markoff: set priority" })
-    nmap("tc", api.set_context, { desc = "Markoff: set context" })
-    nmap("tn", api.new_entry, { desc = "Markoff: new entry" })
+    nmap("D", api.mark_done, { desc = "Markoff: mark done" })
+    nmap("X", api.mark_cancelled, { desc = "Markoff: mark cancelled" })
+    nmap("N", api.new_entry, { desc = "Markoff: new entry" })
     nmap("<C-o>", api.open_list_last, { desc = "Markoff: open last" })
   end,
 })
 
-local markoff = require("markoff")
-
 cc.add("Markoff List", {
   desc = "Show all open TODOs in a picker",
   callback = markoff.open_list,
-})
-
-cc.add("Markoff List Context", {
-  desc = "Show open TODOs filtered by a selected context",
-  callback = markoff.open_list_ask_context,
 })
