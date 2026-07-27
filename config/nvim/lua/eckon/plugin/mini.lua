802411e620f38e23cf7a806ea9800880f7c8daf8
eckon <niklas.meyer95@hotmail.de>
2026-07-27 16:17:18 +0200

    Align neovim status line

--- config/nvim/lua/eckon/plugin/mini.lua
+++ config/nvim/lua/eckon/plugin/mini.lua
@@ -31,6 +31,27 @@ require("mini.indentscope").setup({
   draw = { animation = require("mini.indentscope").gen_animation.linear({ duration = 5 }) },
 })
 
--- basic status line (use mini.git for branch indicator)
-require("mini.statusline").setup()
+-- mainly for the status line git integration
 require("mini.git").setup()
+
+local mini_statusline = require("mini.statusline")
+mini_statusline.setup({
+  content = {
+    active = function()
+      -- taken from the `mini-statusline.txt` default, and removed irrelevant parts for myself
+      local mode, mode_hl = mini_statusline.section_mode({ trunc_width = 120 })
+      local git = mini_statusline.section_git({ trunc_width = 40 })
+      local filename = mini_statusline.section_filename({ trunc_width = 140 })
+
+      return mini_statusline.combine_groups({
+        { hl = mode_hl, strings = { mode } },
+        { hl = "MiniStatuslineDevinfo", strings = { git } },
+        "%<", -- Mark general truncate point
+        { hl = "MiniStatuslineFilename", strings = { filename } },
+        "%=", -- End left alignment
+        { hl = "MiniStatuslineFileinfo", strings = { vim.bo.filetype } },
+        { hl = mode_hl, strings = { "%l:%v" } }, -- line:column
+      })
+    end,
+  },
+})
